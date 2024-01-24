cb37237ae396fa61948b1b8c4c0b7d38402bd458
Drey <oluwadamilareadeniye@yahoo.com>
2024-01-24 15:02:45 -0800

    added student CA and Exam models

--- school_app/models.py
+++ school_app/models.py
@@ -3,6 +3,16 @@ from django.contrib.auth.models import User
 from user_profile.models import Profile
 from school_setting.models import ClassRoom, Subject
 
+CA = (
+    ('Test', 'Test'),
+    ('Class Work', 'Class Work'),
+)
+
+STATUS = (
+    ('Pending', 'Pending'),
+    ('Approved', 'Approved'),
+)
+
 # Create your models here.
 class StudentProfile(models.Model):
     student = models.ForeignKey(Profile, on_delete=models.CASCADE)
@@ -32,3 +42,64 @@ class ParentProfile(models.Model):
 
     def __str__(self):
         return self.parent.user
+    
+
+class ContinuousAssesment(models.Model):
+    ca_type = models.CharField(max_length=10, choices=CA)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    maximum_score = models.PositiveIntegerField(default=0)
+    description = models.TextField(null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+class StudentContinuousAssesmentScore(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    ca = models.ForeignKey(ContinuousAssesment, on_delete=models.CASCADE, verbose_name="Continous Assesment")
+    student_score = models.PositiveIntegerField(default=0)
+    obtainable_score = models.PositiveIntegerField(default=0)
+    student_total_score = models.PositiveIntegerField(default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+class Assignment(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    maximum_score = models.PositiveIntegerField(default=0)
+    description = models.TextField(null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+class StudentAssignmentScore(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE, verbose_name="Assignment")
+    student_score = models.PositiveIntegerField()
+    obtainable_score = models.PositiveIntegerField()
+    student_total_score = models.PositiveIntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+class Examination(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    maximum_score = models.PositiveIntegerField(default=60)
+    exam_date = models.DateTimeField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+class StudentExaminationScore(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    exam = models.ForeignKey(ContinuousAssesment, on_delete=models.CASCADE)
+    student_score = models.PositiveIntegerField(default=0)
+    obtainable_score = models.PositiveIntegerField(default=0)
+    student_total_score = models.PositiveIntegerField(default=0)
+    status = models.CharField(max_length=8, choices=STATUS)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+class StudentTotalGrade(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    ca_score = models.PositiveIntegerField(default=0)
+    exam_score = models.PositiveIntegerField(default=0)
+    total_score = models.PositiveIntegerField(default=0)
+    student_grade = models.CharField(max_length=3, default="Nil")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
